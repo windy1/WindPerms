b6b0af6e5bab8d7b83198517ddd9e098b14cd9ab
Walker <wjcrouse913@aim.com>
2012-04-11 22:46:03 -0400

    MIT

--- src/main/java/net/windwaker/permissions/data/Settings.java
+++ src/main/java/net/windwaker/permissions/data/Settings.java
@@ -1,20 +1,23 @@
-/**
- * The Permissions project.
- * Copyright (C) 2012 Walker Crouse
+/* Copyright (c) 2012 Walker Crouse, http://windwaker.net/
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package net.windwaker.permissions.data;
 
@@ -26,44 +29,40 @@ import net.windwaker.permissions.data.file.FlatFileGroupManager;
 import net.windwaker.permissions.data.file.FlatFileUserManager;
 import net.windwaker.permissions.data.sql.SQLGroupManager;
 import net.windwaker.permissions.data.sql.SQLUserManager;
+
 import org.spout.api.exception.ConfigurationException;
-import org.spout.api.util.config.Configuration;
 import org.spout.api.util.config.ConfigurationHolder;
-import org.spout.api.util.config.ConfigurationNode;
 import org.spout.api.util.config.yaml.YamlConfiguration;
 
 import java.io.File;
-import java.util.logging.Logger;
 
 public class Settings {
 	private static final PermissionsLogger logger = Permissions.getLogger();
 	private static final YamlConfiguration data = new YamlConfiguration(new File("plugins/Permissions/settings.yml"));
-	/*
 	public static final ConfigurationHolder SQL_ENABLED = new ConfigurationHolder(false, "sql.enabled");
 	public static final ConfigurationHolder SQL_PROTOCOL = new ConfigurationHolder("mysql", "sql.protocol");
 	public static final ConfigurationHolder SQL_HOST = new ConfigurationHolder("184.168.194.134", "sql.host");
+	public static final ConfigurationHolder SQL_DATABASE_NAME = new ConfigurationHolder("w1ndwaker, sql.database-name");
 	public static final ConfigurationHolder SQL_USERNAME = new ConfigurationHolder("w1ndwaker", "sql.username");
 	public static final ConfigurationHolder SQL_PASSWORD = new ConfigurationHolder("WalkerCrouse!1", "sql.password");
-	*/
 
 	public static void load() {
 		try {
 			data.load();
-			/*
-			data.addChild(SQL_ENABLED);
-			data.addChild(SQL_PROTOCOL);
-			data.addChild(SQL_HOST);
-			data.addChild(SQL_USERNAME);
-			data.addChild(SQL_PASSWORD);
+			ConfigurationHolder[] nodes = {SQL_ENABLED, SQL_PROTOCOL, SQL_HOST, SQL_DATABASE_NAME, SQL_USERNAME, SQL_PASSWORD};
+			for (ConfigurationHolder node : nodes) {
+				node.setConfiguration(data);
+				node.getValue();
+			}
+			
 			data.save();
-			*/
 		} catch (ConfigurationException e) {
 			logger.severe("Failed to load settings file: " + e.getMessage());
 		}
 	}
 
 	public GroupManager createGroupManager() {
-		if (/*SQL_ENABLED.getBoolean()*/ true) {
+		if (SQL_ENABLED.getBoolean()) {
 			return new SQLGroupManager();
 		}
 
@@ -71,7 +70,7 @@ public class Settings {
 	}
 
 	public UserManager createUserManager() {
-		if (/*SQL_ENABLED.getBoolean()*/ true) {
+		if (SQL_ENABLED.getBoolean()) {
 			return new SQLUserManager();
 		}
 
