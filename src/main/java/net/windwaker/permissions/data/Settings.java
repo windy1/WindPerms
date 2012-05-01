546707ff68dc5b0965d010fde852e2527fbb045c
tenowg <tenowg@gmail.com>
2012-05-01 10:13:31 -0300

    Update src/main/java/net/windwaker/permissions/data/Settings.java

--- src/main/java/net/windwaker/permissions/data/Settings.java
+++ src/main/java/net/windwaker/permissions/data/Settings.java
@@ -42,12 +42,12 @@ import org.spout.api.util.config.yaml.YamlConfiguration;
  */
 public class Settings extends ConfigurationHolderConfiguration {
 	private static final PermissionsLogger logger = Permissions.getLogger();
-	public static final ConfigurationHolder SQL_ENABLED = new ConfigurationHolder(false, "sql.enabled");
-	public static final ConfigurationHolder SQL_PROTOCOL = new ConfigurationHolder("mysql", "sql.protocol");
-	public static final ConfigurationHolder SQL_HOST = new ConfigurationHolder("184.168.194.134", "sql.host");
-	public static final ConfigurationHolder SQL_DATABASE_NAME = new ConfigurationHolder("w1ndwaker", "sql.database-name");
-	public static final ConfigurationHolder SQL_USERNAME = new ConfigurationHolder("w1ndwaker", "sql.username");
-	public static final ConfigurationHolder SQL_PASSWORD = new ConfigurationHolder("WalkerCrouse!1", "sql.password");
+	public static final ConfigurationHolder SQL_ENABLED = new ConfigurationHolder(false, "sql", "enabled");
+	public static final ConfigurationHolder SQL_PROTOCOL = new ConfigurationHolder("mysql", "sql", "protocol");
+	public static final ConfigurationHolder SQL_HOST = new ConfigurationHolder("184.168.194.134", "sql", "host");
+	public static final ConfigurationHolder SQL_DATABASE_NAME = new ConfigurationHolder("w1ndwaker", "sql", "database-name");
+	public static final ConfigurationHolder SQL_USERNAME = new ConfigurationHolder("w1ndwaker", "sql", "username");
+	public static final ConfigurationHolder SQL_PASSWORD = new ConfigurationHolder("WalkerCrouse!1", "sql", "password");
 	
 	private static File file;
 	
